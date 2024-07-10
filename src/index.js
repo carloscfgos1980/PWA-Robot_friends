eb407cdb2dfaca454a1b015d12037b53dde345f8
Carlos Infante <kuva5008@gmail.com>
2024-07-10 04:21:03 +0200

    adding service worker

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { store } from './redux/configureStore';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import * as serviceWorker from './serviceWorker';
 
-// import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,10 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorker.register();
-
-
-
+serviceWorker.register();
